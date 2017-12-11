1cccff53ceb9312f2f529eed263b7245933b19f5
Valsov <Valsov@DESKTOP-54TDIGM>
2017-12-11 18:10:29 +0100

    Server assembly name changed
    From "ReverseShellServerWPF" to "ReverseShellServer"

--- ReverseShellServer/Properties/AssemblyInfo.cs
+++ ReverseShellServer/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReverseShellServerWPF")]
+[assembly: AssemblyTitle("ReverseShellServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ReverseShellServerWPF")]
+[assembly: AssemblyProduct("ReverseShellServer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
