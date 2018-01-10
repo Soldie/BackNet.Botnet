6ee13d8798114ed5ef8c7ea60338d1105cb88218
Valsov <Valsov@DESKTOP-54TDIGM>
2018-01-10 12:47:37 +0100

    TableDisplay()

--- Commands/CommandsManager.cs
+++ Commands/CommandsManager.cs
@@ -125,5 +125,32 @@ namespace Commands
             var keyloggerCommandInstance = (KeyLogger)commandList.Find(x => x.name == "keylogger");
             keyloggerCommandInstance.GetKeyLoggerManagerInstance(manager);
         }
+
+
+        /// <summary>
+        /// Produce a string displaying a table from a list of Tuple(string, string)
+        /// </summary>
+        /// <param name="data">List of tuple to process</param>
+        /// <returns>Table as a string</returns>
+        internal static string TableDisplay(IReadOnlyCollection<Tuple<string, string>> data)
+        {
+            var result = "";
+            var longestPrefix = data.Select(t => t.Item1.Length).Max();
+            var longestValue = data.Select(t => t.Item2.Length).Max();
+            var horizontalDelimiter = $"+{new string('-', longestPrefix + 2)}+{new string('-', longestValue + 2)}+";
+
+            result += horizontalDelimiter + "\n";
+            foreach (var tuple in data)
+            {
+                var spaces = new string(' ', longestPrefix - tuple.Item1.Length);
+                result += $"| {tuple.Item1}{spaces} |";
+
+                spaces = new string(' ', longestValue - tuple.Item2.Length);
+                result += $" {tuple.Item2}{spaces} |\n";
+            }
+            result += horizontalDelimiter;
+
+            return result;
+        }
     }
 }
